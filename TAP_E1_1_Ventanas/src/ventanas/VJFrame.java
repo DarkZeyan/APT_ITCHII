09cd4bfad01c3ac05e76c97230d5d48843895a66
DarkZeyan <esonic0234@hotmail.com>
2023-02-01 13:22:50 -0600

    Modified 1st Practice window title

--- TAP_E1_1_Ventanas/src/ventanas/VJFrame.java
+++ TAP_E1_1_Ventanas/src/ventanas/VJFrame.java
@@ -4,6 +4,8 @@ import javax.swing.JFrame;
 public class VJFrame extends JFrame{
 
     public VJFrame(){
+
+        super("Ventana heredada de JFrame"); // This modifies the window title.
         this.setBounds(100,100,500,300);
         this.setVisible(true);
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);

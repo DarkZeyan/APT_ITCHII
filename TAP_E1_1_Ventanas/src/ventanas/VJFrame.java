0c2fa35cefa2b9ffd892770cb6f9e093b603913b
DarkZeyan <esonic0234@hotmail.com>
2023-02-01 13:30:53 -0600

    Created a class to make the same window without using heritancy

--- TAP_E1_1_Ventanas/src/ventanas/VJFrame.java
+++ TAP_E1_1_Ventanas/src/ventanas/VJFrame.java
@@ -6,7 +6,10 @@ public class VJFrame extends JFrame{
     public VJFrame(){
 
         super("Ventana heredada de JFrame"); // This modifies the window title.
-        this.setBounds(100,100,500,300);
+        // this.setBounds(100,100,500,300);
+
+        //.pack() implementation. This method works to set automatically assign the window bounds with the components inside it.
+        this.pack();
         this.setVisible(true);
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     }

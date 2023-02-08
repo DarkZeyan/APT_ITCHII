be11e26175008ed6bb88e2f8a209f185cc6fbd54
DarkZeyan <esonic0234@hotmail.com>
2023-02-08 13:29:16 -0600

    Fibonacci implementing OOP paradigm

---
+++ TAP_E1_1_Ventanas/src/eventos/FibonacciOOP.java
@@ -0,0 +1,31 @@
+package eventos;
+import javax.swing.JFrame;
+import java.awt.Color;
+import java.awt.Font;
+import javax.swing.JButton;
+import javax.swing.JTextField;
+import javax.swing.JTextArea;
+import javax.swing.JOptionPane;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class FibonacciOOP  extends JFrame{
+
+}
+
+
+class Fibonacci{
+    public Fibonacci() {
+    }
+
+    byte numeroTerminos;
+    public byte getNumeroTerminos() {
+        return numeroTerminos;
+    }
+    public void setNumeroTerminos(byte numeroTerminos) {
+        this.numeroTerminos = numeroTerminos;
+    }
+    
+
+
+}

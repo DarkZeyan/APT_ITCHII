b405e529e27ce778dcee1a9e72f6f6d5b1f88d80
DarkZeyan <esonic0234@hotmail.com>
2023-02-09 16:59:34 -0600

    Finished the FormulaGeneral practice

--- TAP_E1_1_Ventanas/src/eventos/FormulaGeneral.java
+++ TAP_E1_1_Ventanas/src/eventos/FormulaGeneral.java
@@ -35,10 +35,20 @@ public class FormulaGeneral{
         this.cTI = cTI;
     }
 
-    public double x1(){
-        return (((-cTL)+Math.sqrt(Math.pow(cTL,2))-(4*cTC*cTI))/(2*cTC));
+    public double x1() throws NegativeDiscriminatorException{
+        double discriminator=Math.pow(cTL,2)-(4*cTC*cTI);
+        if(discriminator<0) throw new NegativeDiscriminatorException();
+        return (((-cTL)+Math.sqrt(discriminator))/(2*cTC));
     }
-    public double x2(){
-        return (((-cTL)-Math.sqrt(Math.pow(cTL,2))-(4*cTC*cTI))/(2*cTC));
+    public double x2() throws NegativeDiscriminatorException{
+        double discriminator=Math.pow(cTL,2)-(4*cTC*cTI);
+        if(discriminator<0) throw new NegativeDiscriminatorException();
+        return (((-cTL)-Math.sqrt(discriminator))/(2*cTC));
+    }
+}
+
+class NegativeDiscriminatorException extends Exception{
+    public NegativeDiscriminatorException(){
+        super("El discriminante es negativo, este problema no tiene solucion real");
     }
 }
--- TAP_E1_1_Ventanas/src/eventos/FormulaGeneralApp.java
+++ TAP_E1_1_Ventanas/src/eventos/FormulaGeneralApp.java
@@ -25,7 +25,7 @@ public class FormulaGeneralApp extends JFrame{
         createComponentsUsingEvents(this);
         this.setLayout(null);
         this.setResizable(false);
-        this.getContentPane().setBackground(new Color(180,206,235));
+        this.getContentPane().setBackground(new Color(255,33,92));
         this.setBounds(600,600,1000,700);
         this.setLocationRelativeTo(null);
         this.setVisible(true);
@@ -35,7 +35,7 @@ public class FormulaGeneralApp extends JFrame{
 
     public void initComponents(JFrame window) {
 
-        JLabel title = new JLabel("Sucesion de fibonacci");
+        JLabel title = new JLabel("Formula General");
 
         // title.setBounds(450, 50, 300, 30);
         title.setSize(300, 50);
@@ -45,10 +45,10 @@ public class FormulaGeneralApp extends JFrame{
         cTLLbl = new JLabel("Termino Lineal");
         cTILbl = new JLabel("Termino Independiente");
         resultLbl = new JLabel("Resultado");
-        cTCLbl.setBounds(300, 100, 250, 20);
-        cTLLbl.setBounds(300, 100, 250, 20);
-        cTILbl.setBounds(300, 100, 250, 20);
-        resultLbl.setBounds(300, 150, 250, 20);
+        cTCLbl.setBounds(200, 100, 250, 20);
+        cTLLbl.setBounds(200, 140, 250, 20);
+        cTILbl.setBounds(200, 180, 250, 20);
+        resultLbl.setBounds(300, 275, 250, 20);
         title.setFont(new Font("Serif", Font.PLAIN, 24));
         resultLbl.setFont(new Font("Serif", Font.PLAIN, 24));
         cTCLbl.setFont(new Font("Serif", Font.PLAIN, 24));
@@ -61,15 +61,15 @@ public class FormulaGeneralApp extends JFrame{
         window.add(inputCTC);
         inputCTC.setBounds(450, 100, 300, 20);
         inputCTL = new JTextField();
-        inputCTL.setLocation(400, 100);
+        inputCTL.setLocation(400, 140);
         window.add(inputCTL);
-        inputCTL.setBounds(450, 100, 300, 20);
+        inputCTL.setBounds(450, 140, 300, 20);
         inputCTI = new JTextField();
-        inputCTI.setLocation(400, 100);
+        inputCTI.setLocation(400, 180);
         window.add(inputCTI);
-        inputCTI.setBounds(450, 100, 300, 20);
-        result = new JTextArea("Digita un numero arriba para mostrar la serie.");
-        result.setBounds(450, 150, 250, 150);
+        inputCTI.setBounds(450, 180, 300, 20);
+        result = new JTextArea("Digita los coeficientes para imprimir el resultado");
+        result.setBounds(450, 230, 250, 150);
         result.setOpaque(true);
         result.setSize(300, 125);
         result.setEditable(false);
@@ -82,14 +82,67 @@ public class FormulaGeneralApp extends JFrame{
     }
 
     private void createComponentsUsingEvents(JFrame window) {
-        proceed = new JButton("Calcular Serie");
+        proceed = new JButton("Calcular raíces");
         exit = new JButton("Salir");
-        clean = new JButton("Limpiar resultados");
+        clean = new JButton("Limpiar campos");
+
         proceed.setSize(150, 50);
         exit.setSize(150, 50);
+        clean.setSize(150, 50);
 
-        proceed.setLocation(300, 300);
-        exit.setLocation(500, 300);
+        proceed.setLocation(300, 400);
+        exit.setLocation(500, 400);
+        clean.setLocation(400,480);
+
+
+        //Focus TextFields.
+
+        inputCTC.addFocusListener(new FocusListener(){
+
+            @Override
+            public void focusGained(FocusEvent e){
+                inputCTC.setBackground(new Color(115,192,255));
+                inputCTC.setForeground(Color.black);
+            }
+
+            @Override
+            public void focusLost(FocusEvent e){
+                inputCTC.setBackground(new Color(53,109,156));
+                inputCTC.setForeground(Color.white);
+            }
+
+        });
+        inputCTL.addFocusListener(new FocusListener(){
+
+            @Override
+            public void focusGained(FocusEvent e){
+                inputCTL.setBackground(new Color(115,192,255));
+                inputCTL.setForeground(Color.black);
+            }
+
+            @Override
+            public void focusLost(FocusEvent e){
+                inputCTL.setBackground(new Color(53,109,156));
+                inputCTL.setForeground(Color.white);
+            }
+
+        });
+        inputCTI.addFocusListener(new FocusListener(){
+
+            @Override
+            public void focusGained(FocusEvent e){
+                inputCTI.setBackground(new Color(115,192,255));
+                inputCTI.setForeground(Color.black);
+            }
+
+            @Override
+            public void focusLost(FocusEvent e){
+                inputCTI.setBackground(new Color(53,109,156));
+                inputCTI.setForeground(Color.white);
+            }
+
+        });
+    
 
         // Add each button actions.
         proceed.addActionListener(new ActionListener() {
@@ -99,19 +152,26 @@ public class FormulaGeneralApp extends JFrame{
                 try {
                     // Fibonacci fb = new Fibonacci(Byte.parseByte(input.getText()));
 
-                    // if (input.getText() == null || input.getText() == "")
-                    //     throw new NumberFormatException();
-                    // else if (fb.getNumeroTerminos() < 0)
-                    //     throw new Exception("Numero invalido");
+                    if (inputCTC.getText().equals("") || inputCTI.getText().equals("") || inputCTL.getText().equals(""))
+                        throw new Exception();
+                    else{
+                        FormulaGeneral fg = new FormulaGeneral(Byte.parseByte(inputCTC.getText()), Byte.parseByte(inputCTL.getText()), Byte.parseByte(inputCTI.getText()));
+                        result.setText("La raiz 1 es: "+fg.x1()+"\n"+"La raiz 2 es: "+fg.x2());
+                        fg=null;
+                    }
+                    
                     // result.setText(fb.getFibonacciString());
                     // fb = null;
                 } catch (NumberFormatException ex) {
                     JOptionPane.showMessageDialog(window, "La entrada no es un numero, coloca uno e intenta nuevamente",
                             "ERROR", JOptionPane.ERROR_MESSAGE);
 
-                } catch (Exception ex) {
+                }catch(NegativeDiscriminatorException ex){
+                    JOptionPane.showMessageDialog(window, ex.getMessage(),
+                            "ERROR", JOptionPane.ERROR_MESSAGE);
+                }catch (Exception ex) {
                     JOptionPane.showMessageDialog(window,
-                            "La serie no se puede calcular con numeros negativos, intentalo " +
+                            "Uno de los campos esta vacio, llenalo e intentalo " +  
                                     "nuevamente",
                             "ERROR", JOptionPane.ERROR_MESSAGE);
                 }
@@ -129,9 +189,27 @@ public class FormulaGeneralApp extends JFrame{
             }
         });
 
+        clean.addActionListener(new ActionListener(){
+            @Override
+            public void actionPerformed(ActionEvent e){
+                if(result.getText().equals("") && inputCTC.getText().equals("") && inputCTI.getText().equals("") && inputCTL.getText().equals(""))
+                    JOptionPane.showMessageDialog(window,
+                    "Los campos ya estan vacios","ERROR", JOptionPane.ERROR_MESSAGE);
+                else{
+                    inputCTC.setText("");                    
+                    inputCTL.setText("");                    
+                    inputCTI.setText("");                    
+                    result.setText("");
+                }
+            }
+        });
+
         // Add the buttons to the pane
         window.add(proceed);
         window.add(exit);
+        window.add(clean);
     }
-
+    public static void main(String[] args) {
+        new FormulaGeneralApp();
+    }
 }

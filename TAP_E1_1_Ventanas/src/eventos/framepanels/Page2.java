c926a4fb5c9447527fbfa6e942323005deada78f
DarkZeyan <esonic0234@hotmail.com>
2023-02-11 15:26:04 -0600

    Added the constructor to Page2 Class

--- TAP_E1_1_Ventanas/src/eventos/framepanels/Page2.java
+++ TAP_E1_1_Ventanas/src/eventos/framepanels/Page2.java
@@ -7,7 +7,17 @@ import javax.swing.JPanel;
 
 
 
-public class Page2 {
+public class Page2 extends JPanel{
     JLabel title;
-       
+
+    public Page2(){
+        title = new JLabel("Pagina 1");
+        title.setFont(new Font("Serif",Font.BOLD,36));
+        title.setForeground(new Color(255,255,255));
+
+        this.setBackground(new Color(255,33,92));
+        this.setSize(680,420);
+        this.add(title, BorderLayout.CENTER);
+    }
+  
 }

08d15d7443768dff06be3340c601825b99e3fec2
DarkZeyan <esonic0234@hotmail.com>
2023-02-11 15:37:20 -0600

    Added methods to display windows and buttons

--- TAP_E1_1_Ventanas/src/eventos/framepanels/App.java
+++ TAP_E1_1_Ventanas/src/eventos/framepanels/App.java
@@ -18,14 +18,38 @@ public class App  extends JFrame{
         
     }
     
-    public void initButtons(){
+    public void displayButtons(){
         btnPage1 = new JButton("Pagina 1");
         btnPage1.setSize(200,40);
         btnPage1.addActionListener(new ActionListener(){
             @Override
             public void actionPerformed(ActionEvent e){
-                
+                displayWindows(new Page1());
             }
         });
+        btnPage2 = new JButton("Pagina 2");
+        btnPage2.setSize(200,40);
+        btnPage2.addActionListener(new ActionListener(){
+            @Override
+            public void actionPerformed(ActionEvent e){
+                displayWindows(new Page2());
+            }
+        });
+
+
+        btnContainer = new JPanel();
+        btnContainer.setBackground(new Color(30,80,240));
+        btnContainer.setLayout(new FlowLayout(FlowLayout.CENTER));
+        btnContainer.add(btnPage1);
+
+    }
+    private void displayWindows(JPanel window){
+        window.setSize(720,480);
+        window.setLocation(0,0);
+
+        windowContainer.removeAll();
+        windowContainer.add(window,BorderLayout.CENTER);
+        windowContainer.revalidate();
+        windowContainer.repaint();
     }
 }
--- TAP_E1_1_Ventanas/src/eventos/framepanels/MainWindow.java
+++ TAP_E1_1_Ventanas/src/eventos/framepanels/MainWindow.java
@@ -7,5 +7,16 @@ import javax.swing.JPanel;
 
 
 public class MainWindow extends JPanel {
+    private JLabel lblTitle;
+
+    public MainWindow(){
+        lblTitle = new JLabel("Bienvenido al menu principal");
+        lblTitle.setFont(new Font("Serif",Font.BOLD,36));
+        lblTitle.setForeground(Color.white);
+
+
+        this.add(lblTitle,BorderLayout.CENTER);
+        this.setBackground(new Color(140, 60, 255));
+    }
 
 }

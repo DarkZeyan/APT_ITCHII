c60a7bf6b9d00e8312a398ade5b4ce605d6a7941
DarkZeyan <esonic0234@hotmail.com>
2023-02-11 15:27:04 -0600

    Modified the constructor values in Page2

--- TAP_E1_1_Ventanas/src/eventos/framepanels/Page2.java
+++ TAP_E1_1_Ventanas/src/eventos/framepanels/Page2.java
@@ -11,11 +11,11 @@ public class Page2 extends JPanel{
     JLabel title;
 
     public Page2(){
-        title = new JLabel("Pagina 1");
+        title = new JLabel("Bienvenido a la pagina 2");
         title.setFont(new Font("Serif",Font.BOLD,36));
         title.setForeground(new Color(255,255,255));
 
-        this.setBackground(new Color(255,33,92));
+        this.setBackground(new Color(180,70,92));
         this.setSize(680,420);
         this.add(title, BorderLayout.CENTER);
     }

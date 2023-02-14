53272c2f45a9d9acad35a157a18fa07a38089d59
DarkZeyan <esonic0234@hotmail.com>
2023-02-14 08:15:27 -0600

    Modified distancia method and constructor in Recta class

--- TAP_E1_1_Ventanas/src/event_multiple_windows/Recta.java
+++ TAP_E1_1_Ventanas/src/event_multiple_windows/Recta.java
@@ -3,10 +3,10 @@ import java.util.ArrayList;
 public class Recta {
     private Punto punto1;
     private Punto punto2;
-    private ArrayList<Punto> puntos;
 
     public Recta(Punto punto1, Punto punto2){
-
+        setPunto1(punto1);
+        setPunto2(punto2);
     }
     public void setPunto1(Punto punto1) {
         this.punto1 = punto1;
@@ -14,21 +14,14 @@ public class Recta {
     public void setPunto2(Punto punto2) {
         this.punto2 = punto2;
     }
-    public void setPuntos(ArrayList<Punto> puntos) {
-        this.puntos = puntos;
-    }
-
     public Punto getPunto1() {
         return punto1;
     }
     public Punto getPunto2() {
         return punto2;
     }
-    public ArrayList<Punto> getPuntos() {
-        return puntos;
-    }
     public double distancia(){
-        return 1;
+        return Math.sqrt(Math.pow((punto2.getX()-punto1.getX()),2)+Math.pow((punto2.getY()-punto1.getY()),2));
     }
 }   
 

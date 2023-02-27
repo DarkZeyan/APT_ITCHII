8f30bc86aad60c24f2d7adaa61317c39b4720c96
DarkZeyan <esonic0234@hotmail.com>
2023-02-27 13:40:15 -0600

    added the path to the project of the first evaluation

---
+++ EVA1_PROYECTO/src/eva1_proyecto/EVA1_PROYECTO.java
@@ -0,0 +1,20 @@
+/*
+ * 
+ * 
+ */
+package eva1_proyecto;
+
+/**
+ *
+ * @author Jorge Eduardo Escobar Bugarini - ISC - 21550317
+ */
+public class EVA1_PROYECTO {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}

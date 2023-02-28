54e36e1269eb666aa7c2c6d578c8832bc2c91d67
DarkZeyan <esonic0234@hotmail.com>
2023-02-28 08:31:35 -0600

    Modified the Credito class completing its constructor

--- EVA1_PROYECTO/src/modelo/Credito.java
+++ EVA1_PROYECTO/src/modelo/Credito.java
@@ -4,14 +4,35 @@ public class Credito {
     private String numeroCuenta;
     private String numeroTarjeta;
     private double limiteCredito;
-    //private Fecha fechaCreacion;
-    //private Fecha fechaCorte;
-    //private Fecha fechaPago;
+    private Fecha fechaCreacion;
+    private Fecha fechaCorte;
+    private Fecha fechaPago;
     private double tasaInteresAnual;
     private double anualidad;
-    //private Fecha fechaAplicacionAnualidad;
+    private Fecha fechaAplicacionAnualidad;
 
     
+
+    
+
+
+    public Credito(String numeroCuenta, String numeroTarjeta, double limiteCredito, Fecha fechaCreacion,
+            Fecha fechaCorte, Fecha fechaPago, double tasaInteresAnual, double anualidad,
+            Fecha fechaAplicacionAnualidad) {
+        setNumeroCuenta(numeroCuenta);
+        setNumeroTarjeta(numeroTarjeta);
+        setLimiteCredito(limiteCredito);
+        setFechaCreacion(fechaCreacion);
+        setFechaCorte(fechaCorte);
+        setFechaPago(fechaPago);
+        setTasaInteresAnual(tasaInteresAnual);
+        setAnualidad(anualidad);
+        setFechaAplicacionAnualidad(fechaAplicacionAnualidad);
+    }
+
+
+
+
 
     public boolean isActive(){
         boolean activa=false;
@@ -19,15 +40,6 @@ public class Credito {
     }
 
 
-
-    public Credito(String numeroCuenta, String numeroTarjeta, double limiteCredito, double tasaInteresAnual,
-            double anualidad) {
-        setNumeroCuenta(numeroCuenta);
-        setNumeroTarjeta(numeroTarjeta);
-        setLimiteCredito(limiteCredito);
-        setTasaInteresAnual(tasaInteresAnual);
-        setAnualidad(anualidad);
-    }
 
 
 
@@ -87,6 +99,70 @@ public class Credito {
 
     public void setAnualidad(double anualidad) {
         this.anualidad = anualidad;
+    }
+
+
+
+
+
+    public Fecha getFechaCreacion() {
+        return fechaCreacion;
+    }
+
+
+
+
+
+    public void setFechaCreacion(Fecha fechaCreacion) {
+        this.fechaCreacion = fechaCreacion;
+    }
+
+
+
+
+
+    public Fecha getFechaCorte() {
+        return fechaCorte;
+    }
+
+
+
+
+
+    public void setFechaCorte(Fecha fechaCorte) {
+        this.fechaCorte = fechaCorte;
+    }
+
+
+
+
+
+    public Fecha getFechaPago() {
+        return fechaPago;
+    }
+
+
+
+
+
+    public void setFechaPago(Fecha fechaPago) {
+        this.fechaPago = fechaPago;
+    }
+
+
+
+
+
+    public Fecha getFechaAplicacionAnualidad() {
+        return fechaAplicacionAnualidad;
+    }
+
+
+
+
+
+    public void setFechaAplicacionAnualidad(Fecha fechaAplicacionAnualidad) {
+        this.fechaAplicacionAnualidad = fechaAplicacionAnualidad;
     }
     
 }

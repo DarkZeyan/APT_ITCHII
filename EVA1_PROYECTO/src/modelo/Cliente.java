70c0bf1efa3b58a481ca43da8f0fe5d186b28d10
DarkZeyan <esonic0234@hotmail.com>
2023-02-28 08:28:11 -0600

    modified the Cliente class with the new objects

--- EVA1_PROYECTO/src/modelo/Cliente.java
+++ EVA1_PROYECTO/src/modelo/Cliente.java
@@ -1,23 +1,27 @@
 package modelo;
-
+import java.util.ArrayList;
 public class Cliente {
     private String curp;
     private String nombre;
     private String apPaterno;
     private String apMaterno;
-    //private Fecha fechaNacimiento;
-    //private List<Credito> cuentasCredito;
+    private Fecha fechaNacimiento;
+    private ArrayList<Credito> cuentasCredito;
+
 
     
 
     public String getCurp() {
         return curp;
     }
-    public Cliente(String curp, String nombre, String apPaterno, String apMaterno) {
+    public Cliente(String curp, String nombre, String apPaterno, String apMaterno, Fecha fechaNacimiento) {
         setCurp(curp);
         setNombre(nombre);
         setApPaterno(apPaterno);
         setApMaterno(apMaterno);
+        setFechaNacimiento(fechaNacimiento);
+        setCuentasCredito();
+
     }
     public void setCurp(String curp) {
         this.curp = curp;
@@ -40,5 +44,20 @@ public class Cliente {
     public void setApMaterno(String apMaterno) {
         this.apMaterno = apMaterno;
     }
-    
+    public Fecha getFechaNacimiento() {
+        return fechaNacimiento;
+    }
+    public void setFechaNacimiento(Fecha fechaNacimiento) {
+        this.fechaNacimiento = fechaNacimiento;
+    }
+    public ArrayList<Credito> getCuentasCredito() {
+        return cuentasCredito;
+    }
+    public void setCuentasCredito(ArrayList<Credito> cuentasCredito) {
+        this.cuentasCredito = cuentasCredito;
+    }
+    public void setCuentasCredito(){
+        cuentasCredito = new ArrayList<Credito>();
+    }
+        
 }

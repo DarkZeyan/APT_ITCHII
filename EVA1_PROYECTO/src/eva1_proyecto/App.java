ac5fdade3d8dfdfd8eeeb011ef35df2482842121
DarkZeyan <esonic0234@hotmail.com>
2023-03-06 13:41:31 -0600

    Added login screen & app file

---
+++ EVA1_PROYECTO/src/eva1_proyecto/App.java
@@ -0,0 +1,69 @@
+/*
+ * 
+ * 
+ */
+package eva1_proyecto;
+
+/**
+ *
+ * @author Jorge Eduardo Escobar Bugarini - ISC - 21550317
+ * @author Edgar Gerardo Delgado Cerrillo - ISC - 21550297
+ * @author Luis Antonio Garcia Colunga - ISC - 21550740
+ * @author Diego Gael Martin del Campo Loa - ISC - 21550321
+ * @author Sebastian Emilio Murillo Andrade - ISC - 21550384
+ */
+
+//Imports para el manejo de archivos
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+ //Base imports
+
+import java.util.ArrayList;
+import modelo.Usuario;
+
+public class App {
+
+    private static ArrayList<Usuario> usuarios;
+    public static void main(String[] args) {
+        //new LoginScreen();
+    }
+    
+    public static void saveUsers(ArrayList<Usuario> usuarios) {
+        try {
+            //Crear el directorio en caso de que no exista (Caso poco probable ya que es entorno local)
+            String route = "src/resources";
+            File directory = new File(route);
+            if (!directory.exists()) {
+                directory.mkdir();
+            }
+            FileOutputStream openUsersFile = new FileOutputStream(route + "/users.bcny");
+            ObjectOutputStream saveUsersFile = new ObjectOutputStream(openUsersFile);
+            saveUsersFile.writeObject(usuarios);
+            saveUsersFile.flush();
+
+        } catch (IOException e) {
+        }
+    }
+    private static ArrayList<Usuario> getUsuarios(){
+        usuarios = new ArrayList<Usuario>();
+        try {
+
+            FileInputStream openUsersFile = new FileInputStream("src/resources/users.bcny");
+            ObjectInputStream readUsuarios = new ObjectInputStream(openUsersFile);
+            usuarios = (ArrayList<Usuario>) readUsuarios.readObject();
+
+            return usuarios;
+        } catch (FileNotFoundException e) {
+            // No se pudieron encontrar usuarios.  -> Crear nuevos usuarios.
+            usuarios = new ArrayList<Usuario>();
+        } catch (IOException | ClassNotFoundException e) {
+        }
+        return usuarios;
+    }
+    
+}
---
+++ EVA1_PROYECTO/src/vistas/LoginScreen.java
@@ -0,0 +1,11 @@
+package vistas;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+
+public class LoginScreen extends JFrame{
+    public LoginScreen(){
+        super("BANCONY -  INICIO DE SESION");
+        
+    }
+}

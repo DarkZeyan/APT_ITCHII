4cd90095f997c2b37efab7de41d4c169bdbfe33c
DarkZeyan <esonic0234@hotmail.com>
2023-06-08 13:14:55 -0600

    Modelo terminado

--- EVA4_PROYECTO_FINAL/BANCONY/src/com/model/Movimiento.java
+++ EVA4_PROYECTO_FINAL/BANCONY/src/com/model/Movimiento.java
@@ -10,26 +10,16 @@ public class Movimiento {
     private Fecha fechaMovimiento;
     private byte tipoMovimiento;
     private double cantidad;
-    private Tarjeta tarjeta;
-    public Movimiento(int clave, byte tipoMovimiento, double cantidad,Fecha fechaMovimiento,Tarjeta tarjeta) {
+    public Movimiento(int clave, byte tipoMovimiento, double cantidad,Fecha fechaMovimiento) {
         setClave(clave);
         setCantidad(cantidad);
         setTipoMovimiento(tipoMovimiento);
         setFechaMovimiento(fechaMovimiento);
-        setTarjeta(tarjeta);
     }
     public int getClave() {
         return clave;
     }
 
-    public Tarjeta getTarjeta() {
-        return tarjeta;
-    }
-
-    public void setTarjeta(Tarjeta tarjeta) {
-        this.tarjeta = tarjeta;
-    }
-    
     public void setClave(int clave) {
         this.clave = clave;
     }
@@ -52,12 +42,12 @@ public class Movimiento {
         this.fechaMovimiento = fechaMovimiento;
     }   
     public String tipoString(){
-        switch(tipoMovimiento){
-            case 1:return "Ingreso";
-            case 2: return "Retiro";
-            case 3: return "Cargo";
-            default: return "Indefinido";
-        }
+        return switch (tipoMovimiento) {
+            case 1 -> "Ingreso";
+            case 2 -> "Retiro";
+            case 3 -> "Cargo";
+            default -> "Indefinido";
+        };
     }
 }
     

7a0b10c6de95931f6e73f7b0b114d1ba6a6be0da
DarkZeyan <esonic0234@hotmail.com>
2023-06-08 11:33:28 -0600

    Agregada la logica de cuentas de un cliente

--- EVA4_PROYECTO_FINAL/BANCONY/src/com/model/Cliente.java
+++ EVA4_PROYECTO_FINAL/BANCONY/src/com/model/Cliente.java
@@ -7,11 +7,13 @@ import LibreriaFecha.Fecha;
 import LibreriaPersona.CurpInvalida;
 
 import LibreriaPersona.Persona;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Cliente extends Persona{
     
     private int c_cliente;
-
+    private List<Cuenta> cuentas;
     public void setC_cliente(int c_cliente) {
         this.c_cliente = c_cliente;
     }
@@ -19,7 +21,7 @@ public class Cliente extends Persona{
         return c_cliente;
     }
 
-    public Cliente(int c_cliente, String curp, String paterno, String materno, String nombre, String celular, Fecha fechadeNacimiento, String estadoDeNacimiento, char sexo) throws CurpInvalida {
+    public Cliente(int c_cliente, String curp, String paterno, String materno, String nombre, String celular, Fecha fechadeNacimiento, String estadoDeNacimiento, char sexo, List<Cuenta> cuentas) throws CurpInvalida {
         super(curp, paterno, materno, nombre, celular, fechadeNacimiento, estadoDeNacimiento, sexo);
         setC_cliente(c_cliente);
     }
@@ -29,5 +31,16 @@ public class Cliente extends Persona{
         return getCurp()+" "+getNombre()+" "+getPaterno()+" "+getMaterno();
     }
     
-    
+        public List<Cuenta> getCuentas() {
+        return cuentas;
+    }
+
+    public void setTarjetas(List<Cuenta> cuentas) {
+        if(cuentas==null){
+            this.cuentas = new ArrayList<Cuenta>();
+        }else{
+            this.cuentas=cuentas;
+        }
+    }
+
 }

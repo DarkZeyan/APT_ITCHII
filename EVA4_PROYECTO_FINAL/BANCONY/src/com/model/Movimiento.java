88a025ae72afe2a08a5fac9873c8419a09a753f1
DarkZeyan <esonic0234@hotmail.com>
2023-06-08 11:23:28 -0600

    Agregado codigo de movimiento

---
+++ EVA4_PROYECTO_FINAL/BANCONY/src/com/model/Movimiento.java
@@ -0,0 +1,53 @@
+/*
+JORGE EDUARDO ESCOBAR BUGARINI - ISC - 21550317
+ */
+package com.model;
+
+import LibreriaFecha.Fecha;
+
+public class Movimiento {
+    private int clave;
+    private Fecha fechaMovimiento;
+    private byte tipoMovimiento;
+    private double cantidad;
+    
+    public Movimiento(int clave, byte tipoMovimiento, double cantidad,Fecha fechaMovimiento) {
+        setClave(clave);
+        setCantidad(cantidad);
+        setTipoMovimiento(tipoMovimiento);
+        setFechaMovimiento(fechaMovimiento);
+    }
+    public int getClave() {
+        return clave;
+    }
+    public void setClave(int clave) {
+        this.clave = clave;
+    }
+    public byte getTipoMovimiento() {
+        return tipoMovimiento;
+    }
+    public void setTipoMovimiento(byte tipoMovimiento) {
+        this.tipoMovimiento = tipoMovimiento;
+    }
+    public double getCantidad() {
+        return cantidad;
+    }
+    public void setCantidad(double cantidad) {
+        this.cantidad = cantidad;
+    }
+    public Fecha getFechaMovimiento() {
+        return fechaMovimiento;
+    }
+    public void setFechaMovimiento(Fecha fechaMovimiento) {
+        this.fechaMovimiento = fechaMovimiento;
+    }   
+    public String tipoString(){
+        switch(tipoMovimiento){
+            case 1:return "Ingreso";
+            case 2: return "Retiro";
+            case 3: return "Cargo";
+            default: return "Indefinido";
+        }
+    }
+}
+    
